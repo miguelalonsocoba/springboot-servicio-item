72732baa8ace192eb94a995bee9bbc7c11bebbcf
miguelalonsocoba <miguetheking53@gmail.com>
2022-07-07 15:10:04 -0500

    Agregación de metodo getTotal

--- src/main/java/com/formacionbdi/springboot/app/item/models/Item.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Item.java
@@ -10,8 +10,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-	
+
 	private Producto producto;
 	private Integer cantidad;
+
+	public Double getTotal() {
+		return producto.getPrecio() * cantidad.doubleValue();
+	}
 
 }
