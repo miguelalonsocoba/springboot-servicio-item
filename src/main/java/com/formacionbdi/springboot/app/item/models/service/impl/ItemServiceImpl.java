17856aab19d95aa5cea90834a3c0817d4f880448
miguelalonsocoba <miguetheking53@gmail.com>
2022-07-09 09:38:12 -0500

    Refacrorizando la url del servicio que se consume (Productos)

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/impl/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/impl/ItemServiceImpl.java
@@ -23,7 +23,7 @@ public class ItemServiceImpl implements IItemService {
 	@Override
 	public List<Item> findAll() {
 		List<Producto> productos = Arrays
-				.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+				.asList(clienteRest.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
@@ -31,7 +31,7 @@ public class ItemServiceImpl implements IItemService {
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> pathVariables = new HashMap<>();
 		pathVariables.put("id", id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
+		Producto producto = clienteRest.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathVariables);
 		return new Item(producto, cantidad);
 	}
 
