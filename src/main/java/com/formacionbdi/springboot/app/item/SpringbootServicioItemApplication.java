9a1e8b3a70d20418c5cb09d61d9460aeb7098274
miguelalonsocoba <miguetheking53@gmail.com>
2022-07-11 18:36:14 -0500

    Eliminación de la configuración de Ribbon, ya que Eureka aplica por default la configuración de Ribbon

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -3,11 +3,9 @@ package com.formacionbdi.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@RibbonClient(name = "servicio-productos")
 @EnableFeignClients // Habilitar cliente Feign
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
